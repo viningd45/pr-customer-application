b003da1bbfc8f3b5036d81806a3f27b2dfda8437
viningd45 <viningd45@gmail.com>
2023-03-01 09:37:28 -0600

    update default inserts

--- SqlScripts/3. InsertDefaults.sql
+++ SqlScripts/3. InsertDefaults.sql
@@ -12,13 +12,31 @@ INSERT INTO dbo.Customers
 	ZipCodeAdditional
 )
 VALUES
-(   N'Daniel''s Bakery',  -- Name - nvarchar(70)
-    N'3184582814',  -- PhoneNumber - nvarchar(10)
+(   N'Daniel''s Bakery',  
+    N'3184582814',  
 	'123 Fake Ln.',
 	NULL,
 	'Shreveport',
 	'Louisiana',
 	'71047',
+	NULL
+),
+(   N'Superior Grill',  
+    N'3188693243',  
+	'6123 Line Ave.',
+	NULL,
+	'Shreveport',
+	'Louisiana',
+	'71106',
+	NULL
+),
+(   N'Frymaster', 
+    N'3188651711',  
+	'5489 Campus Dr.',
+	NULL,
+	'Shreveport',
+	'Louisiana',
+	'71129',
 	NULL
 )
 
@@ -31,29 +49,111 @@ INSERT INTO dbo.CustomerOpenHours
 )
 VALUES
 (   
-	1,         -- CustomerId - int
-	1,         -- CustomerId - int
-    '08:00', -- Opening - time
-    '17:00'  -- Closing - time
+	1,         
+	1,         
+    '08:00', 
+    '17:00'  
 )
 ,(   
-	1,         -- CustomerId - int
-	2,         -- CustomerId - int
-    '08:00', -- Opening - time
-    '17:00'  -- Closing - time
+	1,         
+	2,         
+    '08:00', 
+    '17:00'  
 ),(   
-	1,         -- CustomerId - int
-	3,         -- CustomerId - int
-    '08:00', -- Opening - time
-    '17:00'  -- Closing - time
+	1,         
+	3,         
+    '08:00', 
+    '17:00'  
 ),(   
-	1,         -- CustomerId - int
-	4,         -- CustomerId - int
-    '08:00', -- Opening - time
-    '17:00'  -- Closing - time
+	1,       
+	4,      
+    '08:00',
+    '17:00' 
 ),(   
-	1,         -- CustomerId - int
-	5,         -- CustomerId - int
-    '08:00', -- Opening - time
-    '17:00'  -- Closing - time
+	1,       
+	5,        
+    '08:00', 
+    '17:00' 
+),
+(   
+	2,         
+	0,         
+    '11:00', 
+    '22:00'  
+),
+(   
+	2,         
+	1,         
+    '11:00', 
+    '22:00'  
+),
+(   
+	2,         
+	2,         
+    '11:00', 
+    '22:00'  
+),
+(   
+	2,         
+	3,         
+    '11:00', 
+    '22:00'  
+),
+(   
+	2,         
+	4,         
+    '11:00', 
+    '22:00'  
+),
+(   
+	2,         
+	5,         
+    '11:00', 
+    '22:00'  
+),
+(   
+	2,         
+	6,         
+    '11:00', 
+    '22:00'  
+),
+(   
+	2,         
+	7,         
+    '11:00', 
+    '22:00'  
+),
+(   
+	3,         
+	1,         
+    '8:00', 
+    '17:00'  
+),
+(   
+	3,         
+	2,         
+    '8:00', 
+    '17:00'  
+),
+(   
+	3,         
+	3,         
+    '8:00', 
+    '17:00'  
+),
+(   
+	3,         
+	4,         
+    '8:00', 
+    '17:00'  
+),
+(   
+	3,         
+	5,         
+    '8:00', 
+    '17:00'  
 )
+
+SELECT * FROM dbo.Customers c
+LEFT JOIN dbo.CustomerOpenHours coo ON coo.CustomerId = c.Id
+ORDER BY c.Id, coo.DayOfWeek
