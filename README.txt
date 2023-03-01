2e1d04b0df43eab45e919032297923efc2ef5e10
Daniel Vining <viningd45@gmail.com>
2023-02-28 21:00:34 -0600

    initial commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Before project launch, run sql scripts in SqlScripts folder in their ordered number.
+Note - database creation may fail depending on local system SQL server express installation path. 
+
+If this is the case, a database named "Consulting" should be created with all default settings. 
+
+The table creation and default data scripts can then be executed along with the application being run.
---
+++ SqlScripts/InsertDefaults.sql
@@ -0,0 +1,59 @@
+USE Consulting
+
+INSERT INTO dbo.Customers
+(
+    Name,
+    PhoneNumber,
+	AddressLineOne,
+	AddressLineTwo,
+	City,
+	State,
+	ZipCode,
+	ZipCodeAdditional
+)
+VALUES
+(   N'Daniel''s Bakery',  -- Name - nvarchar(70)
+    N'3184582814',  -- PhoneNumber - nvarchar(10)
+	'123 Chocolate Ln.',
+	NULL,
+	'Shreveport',
+	'Louisiana',
+	'71047',
+	NULL
+)
+
+INSERT INTO dbo.CustomerOpenHours
+(
+    CustomerId,
+	DayOfWeek,
+    Opening,
+    Closing
+)
+VALUES
+(   
+	1,         -- CustomerId - int
+	1,         -- CustomerId - int
+    '08:00', -- Opening - time
+    '17:00'  -- Closing - time
+)
+,(   
+	1,         -- CustomerId - int
+	2,         -- CustomerId - int
+    '08:00', -- Opening - time
+    '17:00'  -- Closing - time
+),(   
+	1,         -- CustomerId - int
+	3,         -- CustomerId - int
+    '08:00', -- Opening - time
+    '17:00'  -- Closing - time
+),(   
+	1,         -- CustomerId - int
+	4,         -- CustomerId - int
+    '08:00', -- Opening - time
+    '17:00'  -- Closing - time
+),(   
+	1,         -- CustomerId - int
+	5,         -- CustomerId - int
+    '08:00', -- Opening - time
+    '17:00'  -- Closing - time
+)
