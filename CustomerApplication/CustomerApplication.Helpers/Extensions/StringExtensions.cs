c6d6b155c548548795beb190f491696f1f9fcda3
Daniel Vining <viningd45@gmail.com>
2023-02-28 21:06:59 -0600

    add helpers

---
+++ CustomerApplication/CustomerApplication.Helpers/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomerApplication.Helpers.Extensions;
+
+public static class StringExtensions
+{
+    public static bool HasValue(this string value)
+    {
+        return !String.IsNullOrEmpty(value?.Trim());
+    }
+
+    public static string GetNumbers(this string value)
+    {
+        return new (value.Where(char.IsDigit).ToArray());
+    }
+}
---
+++ CustomerApplication/CustomerApplication.Helpers/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerApplication.Helpers.Extensions;
+
+public static class TimeSpanExtensions
+{
+    public static bool Between(this TimeSpan self, TimeSpan start, TimeSpan end)
+    {
+        return self >= start && self <= end;
+    }
+}
